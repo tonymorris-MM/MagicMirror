02cfaecbefbbea7e55e68a3d5d77cb1d354ab0ab
Tony Morris <tmorris@tmorris.net>
2018-10-21 13:46:38 +1000

    tabs to spaces

--- config/config.js
+++ config/config.js
@@ -9,106 +9,106 @@
  */
 
 var config = {
-	address: "localhost", // Address to listen on, can be:
-	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-	                      // - another specific IPv4/6 to listen on a specific interface
-	                      // - "", "0.0.0.0", "::" to listen on any interface
-	                      // Default, when address config is left out, is "localhost"
-	port: 8080,
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
-	                                                       // or add a specific IPv4 of 192.168.1.5 :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+  address: "localhost", // Address to listen on, can be:
+                        // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+                        // - another specific IPv4/6 to listen on a specific interface
+                        // - "", "0.0.0.0", "::" to listen on any interface
+                        // Default, when address config is left out, is "localhost"
+  port: 8080,
+  ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+                                                         // or add a specific IPv4 of 192.168.1.5 :
+                                                         // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+                                                         // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+                                                         // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
-	language: "en",
-	timeFormat: 24,
-	units: "metric",
+  language: "en",
+  timeFormat: 24,
+  units: "metric",
 
-	modules: [
-		{
-			module: "alert",
-		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
-		{
-			module: "clock",
-			position: "top_left",
-			config : {
-				dateFormat: "dddd, DD MMMM YYYY W",
-				displayType: "both",
-				analogSize: "200px",
-				timezone: "Australia/Brisbane"
-			}
-		},
-		{
-			module: "calendar",
-			header: "US Holidays",
-			position: "top_left",
-			config: {
-				calendars: [
-					{
-						symbol: "calendar-check-o ",
-						url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
-					}
-				]
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
-		},
-		{
-			module: "currentweather",
-			position: "top_right",
-			config: {
-				location: "Brisbane",
-				locationID: "2174003",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
-				appid: "66f3367f71ee24f8016100dfdf124802",
-				units: "metric",
-				degreeLabel: true,
-				updateInterval: 3600000,
-				showHumidity: true
-			}
-		},
-		{
-			module: "weatherforecast",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				location: "Brisbane",
-				locationID: "2174003",  //ID from http://www.openweathermap.org/help/city_list.txt
-				appid: "66f3367f71ee24f8016100dfdf124802",
-				units: "metric",
-				degreeLabel: true,
-				updateInterval: 3600000,
-				showHumidity: true,
-				colored: true,
-				tableClass: "large",
-				maxNumberOfDays: 5
-			}
-		},
-		{
-			module: "newsfeed",
-			position: "bottom_bar",
-			config: {
-				feeds: [
-					{
-						title: "SBS World",
-						url: "http://www.sbs.com.au/news/topic/world/feed"
-					},
-					{
-						title: "New York Times",
-						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true
-			}
-		},
-	]
+  modules: [
+    {
+      module: "alert",
+    },
+    {
+      module: "updatenotification",
+      position: "top_bar"
+    },
+    {
+      module: "clock",
+      position: "top_left",
+      config : {
+        dateFormat: "dddd, DD MMMM YYYY W",
+        displayType: "both",
+        analogSize: "200px",
+        timezone: "Australia/Brisbane"
+      }
+    },
+    {
+      module: "calendar",
+      header: "US Holidays",
+      position: "top_left",
+      config: {
+        calendars: [
+          {
+            symbol: "calendar-check-o ",
+            url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
+          }
+        ]
+      }
+    },
+    {
+      module: "compliments",
+      position: "lower_third"
+    },
+    {
+      module: "currentweather",
+      position: "top_right",
+      config: {
+        location: "Brisbane",
+        locationID: "2174003",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
+        appid: "66f3367f71ee24f8016100dfdf124802",
+        units: "metric",
+        degreeLabel: true,
+        updateInterval: 3600000,
+        showHumidity: true
+      }
+    },
+    {
+      module: "weatherforecast",
+      position: "top_right",
+      header: "Weather Forecast",
+      config: {
+        location: "Brisbane",
+        locationID: "2174003",  //ID from http://www.openweathermap.org/help/city_list.txt
+        appid: "66f3367f71ee24f8016100dfdf124802",
+        units: "metric",
+        degreeLabel: true,
+        updateInterval: 3600000,
+        showHumidity: true,
+        colored: true,
+        tableClass: "large",
+        maxNumberOfDays: 5
+      }
+    },
+    {
+      module: "newsfeed",
+      position: "bottom_bar",
+      config: {
+        feeds: [
+          {
+            title: "SBS World",
+            url: "http://www.sbs.com.au/news/topic/world/feed"
+          },
+          {
+            title: "New York Times",
+            url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+          }
+        ],
+        showSourceTitle: true,
+        showPublishDate: true
+      }
+    },
+  ]
 
 };
 
